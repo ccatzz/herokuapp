377e4a95ff7b0329c2f1ee40c4278a8c730db5af
Kevin <ccatzz@yahoo.com>
2019-12-20 13:47:22 +0100

    Impostato template e files

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    scritta="Ciao mondo!"
+    titolo="Pagina iniziale"
+    bottone="Info"
+    testo="Hello World"
     return render_template("base.html", testo=scritta)
 
 @app.route('/info')
 def info():
-    scritta="informazioni"
+    titolo="Pagina Info"
+    bottone="Homepage"
+    testo="Hello World"
+    testo="Informazioni"
     return render_template("base.html", testo=scritta)
 
 if __name__ == '__main__':
